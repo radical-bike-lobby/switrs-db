a8432a9db390a34063f695b1ba1be0da01954140
Benjamin Fry <benjaminfry@me.com>
2024-04-26 10:52:57 -0700

    add victims_view

--- schema/victims.sql
+++ schema/victims.sql
@@ -9,7 +9,6 @@ CREATE TABLE victims (
     victim_safety_equip_1 CHAR(1), -- Victim Safety Equipment 1 (see lookup-tables/victim-tables/VICTIM_SAFETY_EQUIPMENT.csv)
     victim_safety_equip_2 CHAR(1), -- Victim Safety Equipment 2, same as Victim Safety Equipment 1 above (eff. Jan 2002) (see lookup-tables/victim-tables/VICTIM_SAFETY_EQUIPMENT.csv)
     victim_ejected CHAR(1), -- Victim Ejected (see lookup-tables/victim-tables/VICTIM_EJECTED.csv)
-    -- PRIMARY KEY(case_id, party_number, victim_seating_position) -- there is no primary key on this table
     -- add foreign keys
     FOREIGN KEY(case_id, party_number) REFERENCES parties(case_id, party_number)
     FOREIGN KEY(victim_role) REFERENCES victim_role(id)
@@ -22,3 +21,38 @@ CREATE TABLE victims (
 );
 CREATE INDEX idx_victims_case_id ON victims(case_id);
 CREATE INDEX idx_victims_case_id_party_number ON victims(case_id, party_number);
+
+CREATE VIEW victims_view (
+    case_id,
+    party_number,
+    victim_age,
+    -- joined table names
+    victim_role_name,
+    victim_sex_name,
+    victim_degree_of_injury_name,
+    victim_seating_position_name,
+    victim_safety_equip_1_name,
+    victim_safety_equip_2_name,
+    victim_ejected_name
+) AS SELECT 
+    v.case_id,
+    v.party_number,
+    v.victim_age,
+    -- joined table names
+    victim_role.name,
+    victim_sex.name,
+    victim_degree_of_injury.name,
+    victim_seating_position.name,
+    victim_safety_equip_1.name,
+    victim_safety_equip_2.name,
+    victim_ejected.name
+FROM victims AS v
+-- join all the foreign key tables
+LEFT JOIN victim_role ON v.victim_role = victim_role.id
+LEFT JOIN victim_sex ON v.victim_sex = victim_sex.id
+LEFT JOIN victim_degree_of_injury ON v.victim_degree_of_injury = victim_degree_of_injury.id
+LEFT JOIN victim_seating_position ON v.victim_seating_position = victim_seating_position.id
+LEFT JOIN victim_safety_equip victim_safety_equip_1 ON v.victim_safety_equip_1 = victim_safety_equip_1.id
+LEFT JOIN victim_safety_equip victim_safety_equip_2 ON v.victim_safety_equip_2 = victim_safety_equip_2.id
+LEFT JOIN victim_ejected ON v.victim_ejected = victim_ejected.id
+;
