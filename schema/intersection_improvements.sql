15b4ddb46665e15569738326f08634ddff3fffbf
Benjamin Fry <benjaminfry@me.com>
2024-06-02 15:18:15 -0700

    add intersection performance view

--- schema/intersection_improvements.sql
+++ schema/intersection_improvements.sql
@@ -15,7 +15,18 @@ CREATE VIEW intersection_improvements_view (
     date_completed,
     improvement_type,
     -- joined table names
-    improvement_name
+    improvement_name,
+    case_id,
+    before_improvement,
+    collision_datetime,
+    pedestrian_accident,
+    bicycle_accident,
+    number_killed,
+    number_injured,
+    count_ped_killed,
+    count_ped_injured,
+    count_bicyclist_killed,
+    count_bicyclist_injured
 ) AS SELECT 
     i.id,
     i.primary_rd,
@@ -23,8 +34,67 @@ CREATE VIEW intersection_improvements_view (
     i.date_completed,
     i.improvement_type,
     -- joined table names
-    improvement_types.name
+    improvement_types.name,
+    c.case_id,
+    c.collision_datetime < i.date_completed,
+    c.collision_datetime,
+    c.pedestrian_accident,
+    c.bicycle_accident,
+    c.number_killed,
+    c.number_injured,
+    c.count_ped_killed,
+    c.count_ped_injured,
+    c.count_bicyclist_killed,
+    c.count_bicyclist_injured
 FROM intersection_improvements AS i
 -- join all the foreign key tables
 LEFT JOIN improvement_types ON i.improvement_type = improvement_types.id
+LEFT JOIN collisions_view as c ON (c.corrected_primary_rd = i.primary_rd AND c.corrected_secondary_rd = i.secondary_rd)
+                               OR (c.corrected_secondary_rd = i.primary_rd AND c.corrected_primary_rd = i.secondary_rd)
 ;
+
+CREATE VIEW intersection_performance_view (
+    id,
+    primary_rd,
+    secondary_rd,
+    date_completed,
+    improvement_type,
+    -- joined table names
+    improvement_name,
+    case_id,
+    before_improvement,
+    collision_datetime,
+    pedestrian_accident,
+    bicycle_accident,
+    number_killed,
+    number_injured,
+    count_ped_killed,
+    count_ped_injured,
+    count_bicyclist_killed,
+    count_bicyclist_injured
+) AS SELECT 
+    i.id,
+    i.primary_rd,
+    i.secondary_rd,
+    i.date_completed,
+    i.improvement_type,
+    -- joined table names
+    improvement_types.name,
+    c.case_id,
+    c.collision_datetime < i.date_completed,
+    c.collision_datetime,
+    c.pedestrian_accident,
+    c.bicycle_accident,
+    c.number_killed,
+    c.number_injured,
+    c.count_ped_killed,
+    c.count_ped_injured,
+    c.count_bicyclist_killed,
+    c.count_bicyclist_injured
+FROM intersection_improvements AS i
+-- join all the foreign key tables
+LEFT JOIN improvement_types ON i.improvement_type = improvement_types.id
+LEFT JOIN collisions_view as c ON (c.corrected_primary_rd = i.primary_rd AND c.corrected_secondary_rd = i.secondary_rd)
+                               OR (c.corrected_secondary_rd = i.primary_rd AND c.corrected_primary_rd = i.secondary_rd)
+;
+
